ccd97e16cbf46b8226ee43571fee01b1b8fdec9a
Mohammed <el-med7@outlook.com>
2021-02-03 12:01:00 +0100

    finition du js

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,107 @@
-function calculer() 
-{ 
-    let first = document.querySelector("input").value 
-    let second = eval(first) 
-    document.querySelector("input").value = second 
-} 
-//fonction qui affiche la valeur
-function afficher(val) 
-{ 
-    document.querySelector("input").value+=val 
-} 
-//fonction qui efface l'écran 
-function effacer() 
-{ 
-    document.querySelector("input").value = "" 
-} 
+class Calculator {
+    constructor(premierElement, secondElement) {
+        this.premierElement = premierElement
+        this.secondElement = secondElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    ajoutNombre(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    updateDisplay() {
+        this.secondElement.innerText =
+            this.currentOperand
+        if (this.operation != null) {
+            this.premierElement.innerText =
+                `${this.previousOperand} ${this.operation}`
+        } else {
+            this.premierElement.innerText = ''
+        }
+    }
+}
 
 
+const numberButtons = document.querySelectorAll('[data-number]')
+console.log(numberButtons);
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const premierElement = document.querySelector('.previous-operand')
+console.log(premierElement);
+const secondElement = document.querySelector('.current-operand')
 
+const calculator = new Calculator(premierElement, secondElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.ajoutNombre(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
